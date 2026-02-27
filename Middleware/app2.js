6e3774cdd8e4a624766497887243ffea595deafe
patilur <utkarshapatil0145@gmail.com>
2026-02-27 15:30:42 +0530

    routes middleware

---
+++ Middleware/app2.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+// GET /orders
+app.get('/orders', (req, res) => {
+    res.send("Here is the list of all orders.");
+});
+
+// POST /orders
+app.post('/orders', (req, res) => {
+    res.send("A new order has been created.");
+});
+
+// GET /users
+app.get('/users', (req, res) => {
+    res.send("Here is the list of all users.");
+});
+
+// POST /users
+app.post('/users', (req, res) => {
+    res.send("A new user has been added.");
+});
+
+// Start server on port 3000
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
