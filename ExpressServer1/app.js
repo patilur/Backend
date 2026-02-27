aff846b75e261b9a7f2c3c85400bc4e5a411afa5
patilur <utkarshapatil0145@gmail.com>
2026-02-27 13:32:45 +0530

    Express Server

---
+++ ExpressServer1/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use((req, res, next) => {
+    console.log("Server is up and running on port 3000! Ready to handle requests.");
+    //next();
+})
+
+// app.use((req,res,next)=>{
+//     console.log("output from middleware 2");
+//     res.send("<h1>Hello World<h1>")
+// })
+
+app.listen(3000);
