871ee29bf536f09ae5a5089a57a28a4531fef9e0
patilur <utkarshapatil0145@gmail.com>
2026-02-27 17:17:18 +0530

    Dynamic routes

---
+++ Middleware/app4.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/welcome/:username',(req,res)=>{
+    const username=req.params.username;
+    const role=req.query.role;
+
+    res.send(`Welcome ${username},your role is ${role}`);
+})
+
+app.listen(4000, () => {
+    console.log("Server is running on port 4000");
+});
